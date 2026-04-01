de2ab42ded91871c7672f138df93b86188dfb680
Abigail Picone <abigailpicone.ispc@gmail.com>
2026-04-01 16:43:25 -0300

    Se agrega conexión a base de datos MySQL

--- miprimerproyecto/settings.py
+++ miprimerproyecto/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'miprimerproyecto.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'miprimerproyecto_db',
+        'USER': 'root',
+        'PASSWORD': '12345678',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
